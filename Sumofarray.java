2db2ba2daa9097ddd3ef7640fe30efda1bf6e9eb
srinithii <75880775+srinithii@users.noreply.github.com>
2020-12-17 11:30:40 +0530

    Add files via upload

---
+++ Sumofarray.java
@@ -0,0 +1,15 @@
+
+public class Sumofarray {
+public static void main(String[]args) {
+	int numbers[]= {1,2,3,4,5,6};
+	int sum=0;
+	int arrayoflength=array.length;
+	for{i=0; i<arrayoflength; i++);
+{
+	sum=sum+numbers[i];
+	System.out.print(sum);
+}
+}
+	
+}
+}
